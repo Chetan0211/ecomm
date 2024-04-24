738c3395e210fbb3398589529b9038b406d008d1
Chetan0211 <saichetanreddy0211@gmail.com>
2024-04-24 09:51:52 +0530

    Added new category functionality

--- app/controllers/admin/category_controller.rb
+++ app/controllers/admin/category_controller.rb
@@ -1,5 +1,32 @@
-class Admin::CategoryController < ApplicationController
+class Admin::CategoryController < AdminController
     def index
         @categories = Category.all
     end
+
+    def new
+        @new_category = Category.new
+    end
+
+    def create
+        @admin_category = Category.new(admin_category_params)
+
+        respond_to do |format|
+            if @admin_category.save
+                format.html { redirect_to admin_categories_path, notice: "Category was successfully created." }
+                #format.html { redirect_to admin_category_url(@admin_category), notice: "Category was successfully created." }
+                format.json { render :show, status: :created, location: @admin_category }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @admin_category.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+
+    private
+
+    # Only allow a list of trusted parameters through.
+    def admin_category_params
+        params.require(:category).permit(:name, :description)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
 
   get "admin" => "admin#index"
-  get "categories" => "admin/category#index"
-
-  
+  get "categories" => "admin/category#index", as: :admin_categories
+  get "new_category" => "admin/category#new", as: :admin_new_category
+  post "create_category" => "admin/category#create", as: :admin_create_category
 end
